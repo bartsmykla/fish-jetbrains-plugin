bba1468b7bcfd0f5c88e21d42114a9fd74f53117
GitHub Action <action@github.com>
2020-12-28 10:03:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bartsmykla/fishjetbrainsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bartsmykla.fishjetbrainsplugin.services
+
+import com.github.bartsmykla.fishjetbrainsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
